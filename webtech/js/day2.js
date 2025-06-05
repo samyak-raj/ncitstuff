63af3bf08b747951bd1f56f43a0e49111d058c7f
SRS <samyakraj6@gmail.com>
2025-06-05 13:17:32 +0545

    update

--- webtech/js/day2.js
+++ webtech/js/day2.js
@@ -1,41 +1,66 @@
-//Data types
-//number
-let a = 10;
-let b = 12.3;
-console.log(typeof(a))
-console.log(typeof(b))
-//string
-let c = 'A';
-console.log(typeof(c))
-//boolean
-let d = true;
-let e = false;
-console.log(typeof(d))
-console.log(typeof(e))
-//null
-let f = null;
-console.log(f);
-//undefined
-let g = undefined;
-console.log(g);
-//Array => object
-let h = [1, 2, "SRS", 23.3, true]
-console.log(typeof(h));
-//Object
-const i = {
-    "Name": "SRS",
-    "Age": 18,
-    "Gender": "Male"
-}
-console.log(typeof(i))
-console.log(i)
+// //Data types
+// //number
+// let a = 10;
+// let b = 12.3;
+// console.log(typeof(a))
+// console.log(typeof(b))
+// //string
+// let c = 'A';
+// console.log(typeof(c))
+// //boolean
+// let d = true;
+// let e = false;
+// console.log(typeof(d))
+// console.log(typeof(e))
+// //null
+// let f = null;
+// console.log(f);
+// //undefined
+// let g = undefined;
+// console.log(g);
+// //Array => object
+// let h = [1, 2, "SRS", 23.3, true]
+// console.log(typeof(h));
+// //Object
+// const i = {
+//     "Name": "SRS",
+//     "Age": 18,
+//     "Gender": "Male"
+// }
+// console.log(typeof(i))
+// console.log(i)
 
 //operators
 //Arithmetic ( +, -, *, /, %)
-let x = 20, y = 10;
-document.writeln("Sum= ", x + y, "<br>");
-document.writeln("Difference= ", x - y, "<br>");
-document.writeln("Multiply= ", x * y, "<br>");
-document.writeln("Division= ", x / y, "<br>");
-document.writeln("Modulo= ", x % y, "<br>");
-document.writeln("Exponent= ", x ** y, "<br>");
+// let x = 20, y = 10;
+// document.writeln("Sum= ", x + y, "<br>");
+// document.writeln("Difference= ", x - y, "<br>");
+// document.writeln("Multiply= ", x * y, "<br>");
+// document.writeln("Division= ", x / y, "<br>");
+// document.writeln("Modulo= ", x % y, "<br>");
+// document.writeln("Exponent= ", x ** y, "<br>");
+
+// let a = 15, b = 9;
+// console.log(a & b);
+// console.log(a | b);
+// console.log(a ^ b) //XOR
+
+// let num = parseInt(prompt("Enter a numer: "))
+// if ( num % 2 == 0){
+//     document.writeln("Even")
+// } else {
+//     document.writeln("odd")
+// }
+
+// let num = parseInt(prompt("Enter a number: "));
+// if (num > 90 && num <= 100) {
+//     document.writeln("A+")
+// } else if (num > 80 && num <= 90) {
+//     document.writeln("A")
+// } else if (num > 70 && num <= 80) {
+//     document.writeln("B+")
+// } else if (num > 60 && num <= 70) {
+//     document.writeln("B")
+// } else {
+//     document.writeln("Failed")
+// }
