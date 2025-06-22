fbdf2ccc1742e720665e1fbadde8680b950ed773
SRS <samyakraj6@gmail.com>
2025-06-22 15:38:16 +0545

    updates

---
+++ webtech/js/car.js
@@ -0,0 +1,11 @@
+function red(){
+    document.getElementById("image").src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fpremium-psd%2F2d-red-car-left-view_998359-86.jpg&f=1&nofb=1&ipt=0cce9246f260625b3cd6dbc0e8bb8fddc5016f06eb05db5195ea36551b7decfc'
+}
+
+function yellow(){
+    document.getElementById("image").src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6a5mvGehrCijNFIXRKthBAAAAA%26pid%3DApi&f=1&ipt=f77463f2dbbf62b3c0a115ecd5ceb38ff0fbcbc1b3d54d8745cae5b9892ef994&ipo=images'
+}
+
+function blue(){
+    document.getElementById("image").src = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.tk8Iyp_XJQn9pyQzi4gfhgHaDt%26pid%3DApi&f=1&ipt=a2b881ded0b801e33863f1e6abf0e5f2bd492b7a4c02363e387124e16d134b9c&ipo=images'
+}
---
+++ webtech/js/day4.js
@@ -0,0 +1,61 @@
+// a = [10, 20, 30, 40, 50]
+//length
+// document.writeln(a.length)
+//toString
+// b = a.toString();
+// document.writeln(typeof(b))
+
+//Push and POP
+// b = a.pop()
+// document.writeln(b)
+// b = a.push("srs", "apple")
+// document.writeln(b)
+
+//shift and unshift
+// b = a.shift()
+// document.writeln(b)
+// b = a.unshift("srs", "nnnn")
+// document.writeln(b)
+
+//join
+// document.writeln(a.join(", "))
+
+//concat
+// b = ['srs', 'rsrs']
+// c = ['apple', 'ball']
+// document.writeln(a.concat(b, c))
+//delete bad 
+
+// //flat
+// b = [[1,2], [3, 4], [5,6]]
+// document.writeln(b.flat())
+
+// //splice(start, no of deletes, add)
+// let a = [10, 20, 30 , 40 , 50 , 60 , 70, 80 ,90]
+// a.splice(2, 2, "srs");
+// document.writeln(a)
+
+// //slice(start, end)
+// let a = [10, 20, 30 , 40 , 50 , 60 , 70, 80 ,90]
+// b = a.slice(4, 6)
+// document.writeln(b)
+
+//includes
+// document.writeln(a.includes(200))
+
+// function compare(a, b){
+//     return a - b;
+// }
+
+// c = [11, 232, 33, 4424, 8984, 32, 24]
+// b = c.sort(compare)
+// document.writeln(b)
+
+
+// let sum = 0
+// a.forEach(element => {
+//     sum += element
+// });
+// document.writeln(sum)
+
+
